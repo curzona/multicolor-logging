5a3b6494b9c24cb4178496ad026151454f3b9a16
curzona <curzona@gmail.com>
2015-01-24 14:05:52 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='multicolor-logging',
+      description='Multicolor logging for Python',
+      keywords='logging',
+      author='Aron Curzon',
+      author_email='curzona@gmail.com',
+      py_modules=['multicolor'],
+      license='BSD',
+      zip_safe=True,
+      install_requires=['colorama'],
+)
